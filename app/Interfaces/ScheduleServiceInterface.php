0df68803ace480d074ed8b604d637e978ce6501c
lexobegin <65969094+lexobegin@users.noreply.github.com>
2022-02-08 11:12:05 -0400

    myappointments2delete

--- app/Interfaces/ScheduleServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Interfaces;
-
-use Carbon\Carbon;
-
-interface ScheduleServiceInterface 
-{
-	public function isAvailableInterval($date, $doctorId, Carbon $start);
-	public function getAvailableIntervals($date, $doctorId);
-}
